cfc533ee2ade45c17bddf4ab420651482b08b91b
ExtraHash <ExtraHash@protonmail.com>
2020-05-09 09:08:49 -0600

    single instance lock

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 // Modules to control application life and create native browser window
-import { app, BrowserWindow, webContents } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import { createWindow } from './utils/createWindow';
 import log from 'electron-log';
 
 log.info('Launching client...');
+const applicationLock = app.requestSingleInstanceLock()
+
+if (!applicationLock) {
+  log.info('There\'s already an instance locked. Quitting.')
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (BrowserWindow.getAllWindows().length === 1) {
+        const [window] = BrowserWindow.getAllWindows()
+        window.restore();
+        window.focus();
+      }
+  })
+}
 
 app.allowRendererProcessReuse = true;
 app.whenReady().then(createWindow);
